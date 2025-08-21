55aca8216dd791463f26f56e608adf2594b0f564
Mufaqar <mufaqar@gmail.com>
2025-08-21 20:06:21 +0500

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+     rules: {
+      "react/no-unescaped-entities": "off", 
+       "@typescript-eslint/no-explicit-any": "off", 
+    },
   },
 ];
 
