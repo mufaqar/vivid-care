b094912b9c26229c7802830dd55edcc599b998ac
unknown <mufaqar@gmail.com>
2025-10-29 00:30:43 -0700

    Blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,16 +3,6 @@ import { getBlogData } from "@/lib/api/getHomeData";
 
 export default async function DomeCare() {
   const posts = await getBlogData()
-  const banner = {
-    title: "Blogs",
-    featuredImage: {
-      node: {
-        sourceUrl: "/images/default-bg.jpg", // fallback image
-      },
-    },
-    description:
-      "Your trusted source for care, wellness, and lifestyle inspiration. Explore expert advice, personal stories, and helpful guides designed to make everyday living easier and more fulfilling.",
-  };
   return (
     <>
       <AllPosts data={posts} />
