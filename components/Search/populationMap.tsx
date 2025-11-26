faf911e1938953ddffab15b53660e467f29efb0b
Mufaqar <mufaqar@gmail.com>
2025-11-26 21:04:59 +0500

    ap

--- components/Search/populationMap.tsx
+++ components/Search/populationMap.tsx
@@ -4,7 +4,7 @@ export default function ServiceAreasPage() {
   return (
     <section className="py-16">
       <div className="container mx-auto px-4 flex flex-col md:flex-row gap-10">
-        <div className="">
+        <div>
           <InteractiveChoropleth />
         </div>
       </div>
